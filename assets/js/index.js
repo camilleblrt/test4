36cb057c0c26372893f7beca073b450d35f68d2f
camilleblrt <32142095+camilleblrt@users.noreply.github.com>
2017-09-26 09:47:52 -0500

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,35 @@
-$('li a').click(function(e) {
-		    $('a').removeClass('active');
-		    $(this).addClass('active');
-		    });
+		function version33() {
+    document.getElementById("btn-33").style.color="red";
+    document.getElementById("btn-31").style.color="#000";
+    document.getElementById("btn-30").style.color="#000";
+    document.getElementById("btn-29").style.color="#000";
+    document.getElementById("btn-28").style.color="#000";
+  }
+			function version31() {
+    document.getElementById("btn-31").style.color="red";
+    document.getElementById("btn-33").style.color="#000";
+    document.getElementById("btn-30").style.color="#000";
+    document.getElementById("btn-29").style.color="#000";
+    document.getElementById("btn-28").style.color="#000";
+  }
+			function version30() {
+    document.getElementById("btn-30").style.color="red";
+    document.getElementById("btn-33").style.color="#000";
+    document.getElementById("btn-31").style.color="#000";
+    document.getElementById("btn-29").style.color="#000";
+    document.getElementById("btn-28").style.color="#000";
+  }
+			function version29() {
+    document.getElementById("btn-29").style.color="red";
+    document.getElementById("btn-33").style.color="#000";
+    document.getElementById("btn-31").style.color="#000";
+    document.getElementById("btn-30").style.color="#000";
+    document.getElementById("btn-28").style.color="#000";
+  }
+			function version28() {
+    document.getElementById("btn-28").style.color="red";
+    document.getElementById("btn-31").style.color="#000";
+    document.getElementById("btn-30").style.color="#000";
+    document.getElementById("btn-29").style.color="#000";
+    document.getElementById("btn-33").style.color="#000";
+  }
